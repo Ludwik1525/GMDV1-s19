387b2e2cabab33084b63239ce5a864333a624367
Unknown <266128@via.dk>
2019-02-16 14:48:03 +0100

    Assets for weapons, bullets, sky etc...

---
+++ Maze_shooter/Assets/WeaponScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+        Transform bTransform = bullet.transform;
+
+        bTransform.position = bulletSpawnPoint.position;
+        bTransform.forward = transform.up;
+
+        
+    }
+}
