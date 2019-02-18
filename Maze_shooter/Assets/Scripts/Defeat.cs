ac117c4eb079996450422cef1b6143dc578a5d54
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2019-02-18 11:39:27 +0100

    Script for changing scene to death/defeat
    checks for a collision with "Enemy" we could make it a tag for easier debugging later

---
+++ Maze_shooter/Assets/Scripts/Defeat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Defeat : MonoBehaviour {
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Enemy")
+        {
+        //SceneManager.LoadScene("You were not prepared, scene");
+        SceneManager.LoadScene("DefeatScene");
+
+        }
+    }
+}
