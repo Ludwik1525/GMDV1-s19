ed4462e66d30f84069deaa033d3ced8aa04c179f
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2019-02-19 13:39:33 +0100

    wall collision script
    script that locks the velocity of the rigidbody on the gameobject to 0 on the "oncollisionenter" and "oncollisionstay"

---
+++ Maze_shooter/Assets/Scripts/PlayerWallCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallCollision : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        rb.angularVelocity = Vector3.zero;
+    }
+}
+}
