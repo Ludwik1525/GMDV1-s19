276a31fe67097d2d3d91ce1d0faf39cb7600e8da
Ludwik1526 <mmichal1526@gmail.com>
2019-02-21 00:31:45 +0100

    Final fixes

---
+++ Maze_shooter/Assets/Scripts/EnemyActivate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActivate : MonoBehaviour {
+
+    [SerializeField]
+    GameObject enemy1;
+    [SerializeField]
+    GameObject enemy2;
+
+    void Start () {
+
+        enemy1.SetActive(false);
+        enemy2.SetActive(false);
+    }
+
+    bool areActive = false;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!areActive)
+        {
+            areActive = true;
+            enemy1.SetActive(true);
+            enemy2.SetActive(true);
+        }
+    }
+}
