c98dfdf391da95cd8531d44ca8806f52a39a74d1
Simon Bech Jensen <life.lime@gmail.com>
2019-02-20 19:52:40 +0100

    hopefully i added all the prefabs now

---
+++ Maze_shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform playerPos;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        navMeshAgent.destination = playerPos.position;
+	}
+}
---
+++ Maze_shooter/Assets/Scripts/SprayControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayControl : MonoBehaviour {
+
+    public GameObject spray;
+
+    private GameObject tempSpray;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            tempSpray = GameObject.Instantiate(spray);
+            tempSpray.transform.parent = transform;
+            tempSpray.transform.localEulerAngles = new Vector3(175, 0, 0);
+            tempSpray.transform.localPosition = new Vector3(0,0,0);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            GameObject.Destroy(tempSpray);
+        }
+
+
+    }
+}
