ac4c233612d4182318fa9cddf0d599c89924ddab
Ludwik1526 <mmichal1526@gmail.com>
2019-02-20 21:39:29 +0100

    Unused files deleted, first person view

---
+++ Maze_shooter/Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera frontCamera;
+
+    void Start()
+    {
+        frontCamera.enabled = false;
+        mainCamera.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            frontCamera.enabled = !frontCamera.enabled;
+        }
+    }
+
+
+}
